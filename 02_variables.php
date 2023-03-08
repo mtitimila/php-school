2bd76b4cac3966a232de14d650e86e1f19fd0473
Mtitimila <nafelinojaccobs@gmail.com>
2023-03-08 11:12:04 +0200

    first commit on variables

---
+++ 02_variables.php
@@ -0,0 +1,22 @@
+<?php
+
+/*-----------------Php Data <types----------------*/
+
+/*
+-String - Series of characters
+-Integer -
+-FLoat
+-Object
+-Null
+-Object
+
+*/
+
+//Always use under score format
+
+
+$name = 'Alinafe Mtitimila';
+
+$age = 27;
+
+$married = false;
