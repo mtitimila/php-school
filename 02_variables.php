9fc25835d76f57b4a09e156c739151c7d578e550
Mtitimila <nafelinojaccobs@gmail.com>
2023-03-08 15:42:40 +0200

    arrays

--- 02_variables.php
+++ 02_variables.php
@@ -20,3 +20,39 @@ $name = 'Alinafe Mtitimila';
 $age = 27;
 
 $married = false;
+
+$cash_on_hand = 39.05;
+
+
+// echo $married;
+// var_dump($married);
+// echo $cash_on_hand;
+
+
+//string format
+
+//echo $name . ' is ' . $age . ' years old';
+
+//echo "${name} is '${age}' years old";
+
+// echo "$name is $age years old";
+
+// echo 5 + 5;
+
+// $x =  '5' + '5';
+
+// var_dump($x);
+
+// echo 10 - 5;
+
+// echo 5 * 6;
+
+// echo 10 % 3;
+
+
+// constants
+
+define('HOST', 'localhost');
+define('DB_NAME', 'database');
+
+echo HOST;
---
+++ 03_arrays.php
@@ -0,0 +1,62 @@
+<?php
+
+$numbers = [1, 44, 55, 22];
+
+$fruits = array('mango', 'orange', 'orange');
+
+//print_r($fruits);
+
+// echo $fruits[1];
+
+
+//associative array
+
+// $colors = [
+//     1 => 'red',
+//     2 => 'green',
+//     3 => 'yellow'
+// ];
+
+
+// echo $colors[1];
+
+
+// $hex = [
+//     'red' => '#FFFFFF',
+//     'blue' => '#FFFFFF',
+//     'green' => '#FFFFFF',
+// ];
+
+
+// echo $hex['red'];
+
+$person = [
+    'first_name' => 'John',
+    'last_name' => 'Doe',
+    'email' => 'john@example.com',
+];
+
+$people = [
+    [
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'email' => 'john@example.com',
+    ],
+
+    [
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'email' => 'john@example.com',
+    ],
+
+    [
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'email' => 'john@example.com',
+    ]
+];
+
+
+// echo $people[1]['first_name'];
+
+// var_dump(json_encode($people));
