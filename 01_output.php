98292e74046482a8d32c2869a8bd2d3cf26d57e7
Mtitimila <nafelinojaccobs@gmail.com>
2023-03-08 11:07:47 +0200

    outputs done

--- 01_output.php
+++ 01_output.php
@@ -1,4 +1,27 @@
 <?php
 
-echo "First output that i have made in this journey";
-echo "Ahh ahh";
+/*
+This file contains about printing output functions
+for php code
+*/
+
+//echo -print string and what not
+
+echo 123, 'hello', 10.5;
+
+
+//print- only a single value
+
+
+print 'hello';
+
+
+//print_r - prints arrays
+
+
+print_r([1, 2, 3, 4, 5]);
+
+
+//var_dump - prints alot of information of the variables
+
+var_dump([1, 2, 3, 4, 5]);
